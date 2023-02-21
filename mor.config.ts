b0b9ea1e2513bda5a7fd8ac97fa8abc721668399
致康 <zhikangliu.lzk@alibaba-inc.com>
2023-02-21 22:48:25 +0800

    feat: 测试 mor init 功能

---
+++ mor.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from '@ali/openmor-cli'
+
+export default defineConfig([
+  {
+    name: 'wechat-miniprogram',
+    sourceType: 'wechat',
+    target: 'wechat',
+    compileType: 'miniprogram',
+    compileMode: 'bundle'
+  },
+  {
+    name: 'alipay-miniprogram',
+    sourceType: 'wechat',
+    target: 'alipay',
+    compileType: 'miniprogram',
+    compileMode: 'bundle'
+  }
+])
